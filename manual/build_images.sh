6052063f85cd1eaf69717f76f49ce36d7006475f
Daniel Glazer <danielglazer200026@gmail.com>
2023-12-15 00:27:15 +0100

    Add manual deployment

---
+++ manual/build_images.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker build -t central-module:latest ../../central-module/ --no-cache
+docker build -t calculations-db-access:latest ../../calculationsdb-access/ --no-cache
+docker build -t mediative-module:latest ../../mediative-module/  --no-cache
+docker build -t simulation-module:latest ../../simulation-module/ --no-cache
+echo "Building of images has been completed"
+
+
+
---
+++ manual/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$#" -lt 1 ]; then
+    echo "You have to pass ssh configuration for example 'ubuntu@ip_address -p <PORT>'"
+    exit 1
+fi
+
+ssh_configuration="$1"
+
+echo "Start uploading central-module"
+docker save central-module:latest | ssh -C "$ssh_configuration" docker load
+echo "Start uploading  calculations-db-access"
+docker save calculations-db-access:latest | ssh -C "$ssh_configuration" docker load
+echo "Start uploading mediative-module"
+docker save mediative-module:latest | ssh -C "$ssh_configuration" docker load
+echo "Start uploading simulation-module"
+docker save simulation-module:latest | ssh -C "$ssh_configuration" docker load
+echo "Uploaded all images"
+
+ssh "$ssh_configuration" 'mkdir -p deploy/'
+scp docker-compose-manual.yaml "$ssh_configuration":deploy/
+ssh "$ssh_configuration" 'docker compose -f deploy/docker-compose-manual.yaml up'
